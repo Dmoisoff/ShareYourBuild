be149c2765cdb49da852bbb8945ba6b6b089669e
Daniel Moisoff <dmoisoff@gmail.com>
2018-07-10 17:23:53 -0400

    Add webpack-cli

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,7 +22,7 @@ class User < ApplicationRecord
 
   after_initialize :ensure_session_token
 
-
+# test
 
   def self.find_by_credentials(username, password)
     user = User.find_by(username: username)
