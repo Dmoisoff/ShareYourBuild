592f3d600f6c56f011ba17f59ad4ad9c90e074ec
Daniel Moisoff <dmoisoff@gmail.com>
2018-07-15 17:54:47 -0400

    Fix blur offset session pages

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,6 +1,8 @@
 class Api::UsersController < ApplicationController
   def create
     @user = User.new(user_params)
+    defaultProfilePicture = File.open('app/assets/images/user_profile_pic.png')
+    @user.picture.attach(io: defaultProfilePicture, filename: 'user_profile_pic.png')
     if @user.save
       log_in!(@user)
       render "api/users/show"
